7a1214a171b6f91d538de47435484d6f571f483e
Shiv Nandan <36573508+ShivNandan-Coder@users.noreply.github.com>
2020-10-24 10:24:29 +0530

    Google-firebase.json
    when you create Firebase Project then download google-firebase.json

---
+++ main/java/com/example/firebasenotification/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.firebasenotification;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.nfc.Tag;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel=
+                    new NotificationChannel("MyNotifications","MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+       NotificationManager manager= getSystemService(NotificationManager.class);
+       manager.createNotificationChannel(channel);
+
+        }
+        FirebaseMessaging.getInstance().subscribeToTopic("general")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg="Successfull";
+                        if(!task.isSuccessful())
+                        {
+                            msg="failed";
+                        }
+                     //   Log.d(Tag,msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+    }
+}
---
+++ main/java/com/example/firebasenotification/MyMessaingService.java
@@ -0,0 +1,26 @@
+package com.example.firebasenotification;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyMessaingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+    //Notification generate Wala Code hai
+    public void showNotification(String title,String message)
+    {
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"MyNotifications")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(message);
+        NotificationManagerCompat manager=NotificationManagerCompat.from(this);
+        manager.notify(999,builder.build());
+    }
+}
